81ec7f6f6f4f0b82d9c026bdf23589671def4927
Ajax12345 <jpetullo14@gmail.com>
2025-02-13 21:02:37 -0500

    udpates

--- llmdb.py
+++ llmdb.py
@@ -8,15 +8,33 @@ def parse_schema(workload:str) -> typing.List:
     return sqlglot.parse(tbl_schema)
     
 def parse_workload(workload:str) -> typing.List:
-    def tpch() -> typing.List:
+    def tpch() -> dict:
         with open('tpch_schema/queries.sql') as f:
             return {f'q{i}.sql': sqlglot.parse(a)[0] 
                         for i, a in enumerate(f, 1) if a}
 
-    def tpcds() -> None:
-        raise NotImplementedError('queries have not yet been added for TPC-DS')
+    def tpcds() -> dict:
+        d = {}
+        p = 'tpcds_schema/queries'
 
-    def job() -> typing.List:
+        class ErrWrapper:
+            def __init__(self, sql:str) -> None:
+                self._sql = sql
+            
+            def sql(self) -> str:
+                return self._sql
+            
+    
+        for i in os.listdir(p):
+            if re.findall('\.sql$', i):
+                with open(os.path.join(p, i)) as f:
+                    try:
+                        d[i] = sqlglot.parse(src:=f.read())[0]
+                    except sqlglot.errors.ParseError:
+                        d[i] = ErrWrapper(src)
+        return d
+
+    def job() -> dict:
         d = {}
         for i in os.listdir('job_schema'):
             if re.findall('^\d+\w\.sql', i):
@@ -33,7 +51,5 @@ def parse_workload(workload:str) -> typing.List:
     }[workload]()
 
 if __name__ == '__main__':
-    l = parse_workload('job')
-    for a, b in l.items():
-        print(a)
-        print(b.sql())
+    l = parse_workload('tpcds')
+    print(l['query1.sql'].sql())
