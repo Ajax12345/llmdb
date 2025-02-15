8807d1a9478c8249e10e8edb33fda9ba72a2961f
Ajax12345 <jpetullo14@gmail.com>
2025-02-14 20:41:58 -0500

    updates

--- llmdb.py
+++ llmdb.py
@@ -1,6 +1,11 @@
 import typing, collections
 import sqlglot, os, re
 import db_gpt, json
+import sklearn.cluster
+import sklearn.manifold
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
 
 def parse_schema(workload:str) -> typing.List:
     with open(os.path.join(f'{workload}_schema', 'schema.sql')) as f:
@@ -66,7 +71,87 @@ def vectorize_schema(workload:str) -> None:
         json.dump({a:db_gpt.get_embedding(db_gpt.CLIENT, b.sql()) 
             for a, b in parse_workload_schema(workload).items()}, f)
 
+def load_workload_embeddings(workload:str) -> dict:
+    with open(f'{workload}_schema/query_embeddings.json') as f:
+        return json.load(f)
+
+def load_workload_schema_embeddings(workload:str) -> dict:
+    with open(f'{workload}_schema/schema_embeddings.json') as f:
+        return json.load(f)
+
+class Workload:
+    def __init__(self, workload:str) -> None:
+        self.workload = workload
+        self.queries = parse_workload(workload)
+        self.tables = parse_workload_schema(workload)
+        self._query_embeddings = load_workload_embeddings(workload)
+        self._table_embeddings = load_workload_schema_embeddings(workload)
+        assert self.query_num == len(self._query_embeddings)
+        assert self.table_num == len(self._table_embeddings)
+
+    @property
+    def query_embeddings(self) -> typing.List[list]:
+        return [self._query_embeddings[i] for i in sorted(self._query_embeddings)]
+
+    @property
+    def table_num(self) -> int:
+        return len(self.tables)
+    
+    @property
+    def query_num(self) -> int:
+        return len(self.queries)
+
+    @property
+    def meta(self) -> dict:
+        return {
+            'workload': self.workload,
+            'queries': self.query_num,
+            'tables': self.table_num,
+        }
+
+    def __repr__(self) -> str:
+        return f'<{str(self.meta)}>'
+    
+    
+    def display_query_clusters(self) -> None:
+        v = [
+            ('kmeans', sklearn.cluster.KMeans(n_clusters=self.table_num, init="k-means++", random_state=42)),
+            ('AggCluster(cosine,avg)', sklearn.cluster.AgglomerativeClustering(n_clusters=self.table_num, metric='cosine', linkage = 'average')),
+            ('AggCluster', sklearn.cluster.AgglomerativeClustering(n_clusters=self.table_num)),
+            ('DBSCAN(0.1)', sklearn.cluster.DBSCAN(eps = 0.1, metric = 'cosine'))
+        ]
+        
+        #kmeans = 
+        
+        fig, plts = plt.subplots(nrows=2, ncols=2)
+        for p in plts:
+            for P in p:
+                t, kmeans = v.pop(0)    
+                kmeans.fit(matrix:=self.query_embeddings)
+                print(kmeans.labels_)
+                d = collections.defaultdict(list)
+                for i, a in enumerate(kmeans.labels_):
+                    d[a].append(i)
+
+                tsne = sklearn.manifold.TSNE(n_components=2, perplexity=15, random_state=42, init="random", learning_rate=200)
+                vis_dims2 = tsne.fit_transform(np.array(matrix))
+
+                x = [x for x, y in vis_dims2]
+                y = [y for x, y in vis_dims2]
+
+                COLORS = ['brown', 'red', 'cadetblue', 'greenyellow', 'moccasin', 'slateblue', 'blueviolet', 'gray', 'navy', 'yellow', 'deepskyblue', 'teal', 'forestgreen', 'orange', 'violet', 'sienna', 'turquoise', 'black', 'darkkhaki', 'purple', 'orchid', 'rosybrown', 'olive', 'silver', 'maroon']
+                for i, color in enumerate(COLORS[:self.table_num]):
+                    xs = [x[j] for j in d[i]]
+                    ys = [y[j] for j in d[i]]
+                    P.scatter(xs, ys, color=color, alpha=0.3)
+                
+                P.set_title(t)
+
+        plt.suptitle(f'Workload: {self.workload.upper()}')
+        plt.show()
+
 
 if __name__ == '__main__':
     #vectorize_workload('tpcds')
-    vectorize_schema('job')
+    w = Workload('job')
+    w.display_query_clusters()
